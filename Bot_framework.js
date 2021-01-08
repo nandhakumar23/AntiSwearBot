67ea7ac22b5d586c368aa8988b95d3d3c8f00165
Nandhakumar Saravana <nsaravan@usc.edu>
2021-01-07 22:33:22 -0800

    Got most done, need to look into vosk and neatly presenting data to disc

--- Bot_framework.js
+++ Bot_framework.js
@@ -2,7 +2,17 @@ const Discord = require("discord.js");
 const { execSync } = require("child_process");
 const { token} = require('./disc_config.json');
 const fs = require('fs');
+const { Readable } = require('stream')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg')
+ffmpeg.setFfmpegPath(ffmpegPath);
+const { exec } = require('child_process');
 
+class Silence extends Readable {
+    _read() {
+        this.push(Buffer.from([0xF8, 0xFF, 0xFE]))
+    }
+}
 
 const client = new Discord.Client();
 var isInVoice = false;
@@ -23,26 +33,55 @@ client.on('message', async (message) => {
     }
 
     // Join the same voice channel of the author of the message
-    if (message.member.voice.channel && message.content === 'join') {
+    if (message.member.voice.channel && message.content === '+join') {
 
         const user = message.member.id;
         connection = await message.member.voice.channel.join();
 
         isInVoice = true;
-        // Create a ReadableStream of s16le PCM audio
-        const receiver = connection.receiver.createStream(message.member, {
-            mode: "pcm",
-            end: "silence"
-        });
 
-        const writer = receiver.pipe(fs.createWriteStream(`./recorded-${message.author.id}.pcm`));
-        writer.on("finish", () => {
-            message.member.voice.channel.leave();
-            message.channel.send("Finished writing audio");
+        connection.play(new Silence(), { type: 'opus' })
+
+        connection.on('speaking', (user, speaking) => {
+            console.log(user.username, 'speaking:', speaking);
+
+            if(speaking.bitfield == 0) return;
+
+            // Create a ReadableStream of s16le PCM audio
+            const receiver = connection.receiver.createStream(user, {
+                mode: "pcm",
+                end: "silence"
+            });
+
+            const writer = receiver.pipe(fs.createWriteStream(`./recorded-${user.id}.pcm`));
+            writer.on("finish", function () {
+                ffmpeg(`./recorded-${user.id}.pcm`)
+                    .inputOptions(['-f s16le', '-ar 48000', '-channels 2'])
+                    .outputOptions(['-f wav', '-ar 16000', '-ac 1'])
+                    .save(`./recorded-${user.id}.wav`)
+                    .on('end', function () {
+                        exec(`python SpeechRec.py recorded-${user.id}.wav`, (error, stdout, stderr) => {
+
+                            if (error) {
+                                console.error(`error: ${error.message}`);
+                                return;
+                            }
+
+                            if (stderr) {
+                                console.error(`stderr: ${stderr}`);
+                                return;
+                            }
+                            var output = stdout
+                            if (output.text === "") return;
+                            console.log(user.username, ": ", output.toString());
+                            message.channel.send(user.username + ": " + output.toString());
+                        });
+                    })
+            });
         });
     }
 
-    if(message.content === 'Run it back'){ // temp command to re-play the listen to the audio that u just said.
+    if(message.content === '+Run it back'){ // temp command to re-play the listen to the audio that u just said.
         //connection.play('user_audio', {mode: 'opus'});
 
         const voicechannel = message.member.voice.channel;
@@ -63,7 +102,7 @@ client.on('message', async (message) => {
 
     }
 
-    if(isInVoice && message.content === 'dc'){
+    if(isInVoice && message.content === '+dc'){
         connection.disconnect();
         connection = null
         console.log("cleared");
--- SpeechRec.py
+++ SpeechRec.py
@@ -1,26 +1,86 @@
 import speech_recognition as sr
 from os import path
+import sys
+import soundfile as sf
+import pyloudnorm as pyln
+import json
+from better_profanity import profanity
 
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)),"input.wav")
+def use_spinx():
+    try:
+        text = Object_data['Sphinx']
+        IsProfanity = profanity.contains_profanity(text)
+        Object_data['Profanity'] = IsProfanity
+    except:
+        Object_data['silence']="false"
+
+
+
+THRESHOLD = -46
+Object_data = {}
+Object_data['silence'] = "false"
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
 
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
 
-# recognize speech using Sphinx
+data, rate = sf.read(sys.argv[1]) # load audio
+
+meter = pyln.Meter(rate) # create BS.1770 meter
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+    loudness = meter.integrated_loudness(data)
+    print("Loudness: ", loudness)
+    if(loudness < THRESHOLD):
+        print("Volume too low or silence, Skipping!")
+        Object_data['silence'] = "true"
+        exit()
+except ValueError:
+    print("Volume too low or silence, Skipping! (In this case file was too small)")
+    exit()
+
 
 f = open("wit_api.txt", "r")
 WIT_AI_KEY = f.read()
 
 try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+    Wit_Output = r.recognize_wit(audio, key=WIT_AI_KEY)
+    print("Wit.ai thinks you said " + Wit_Output)
+    Object_data['Wit']= Wit_Output
 except sr.UnknownValueError:
-    print("Wit.ai could not understand audio")
+    Wit_Output = "Wit.ai could not understand audio"
+    print(Wit_Output)
+    #Object_data['Wit'] = Wit_Output
 except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
+    Wit_Output = "Could not request results from Wit.ai service"
+    print(Wit_Output)
+    #Object_data['Wit'] = Wit_Output
+
+
+# recognize speech using Sphinx
+try:
+    Sphinx_Output = r.recognize_sphinx(audio)
+    print("Sphinx thinks you said " + Sphinx_Output)
+    Object_data['Sphinx'] = Sphinx_Output
+except sr.UnknownValueError:
+    Sphinx_Output = "Sphinx could not understand audio"
+    print(Sphinx_Output)
+    #Object_data['Sphinx'] = Sphinx_Output
+except sr.RequestError as e:
+    Sphinx_Output = "Sphinx Error"
+    print(Sphinx_Output)
+    #Object_data['Sphinx'] = Sphinx_Output
+
+try:
+    if Object_data['silence']=="false":
+        text = Object_data['Wit']
+        IsProfanity = profanity.contains_profanity(text)
+        Object_data['Profanity'] = IsProfanity
+except:
+    use_spinx()
+
+final_json=json.dumps(Object_data)
+print(final_json)
+
+
+
