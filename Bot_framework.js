b4a0f9af6de02172ed3a45e24e466af5c9e9c459
Nandhakumar Saravana <nsaravan@usc.edu>
2021-01-11 22:31:03 -0800

    Core implementation done

--- Bot_framework.js
+++ Bot_framework.js
@@ -5,8 +5,11 @@ const fs = require('fs');
 const { Readable } = require('stream')
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg')
+const readJsonSync = require('read-json-sync');
 ffmpeg.setFfmpegPath(ffmpegPath);
 const { exec } = require('child_process');
+
+
 
 class Silence extends Readable {
     _read() {
@@ -35,51 +38,142 @@ client.on('message', async (message) => {
     // Join the same voice channel of the author of the message
     if (message.member.voice.channel && message.content === '+join') {
 
+
         const user = message.member.id;
         connection = await message.member.voice.channel.join();
+        var ConnectedChannel = message.member.voice.channel
 
         isInVoice = true;
 
         connection.play(new Silence(), { type: 'opus' })
 
         connection.on('speaking', (user, speaking) => {
-            console.log(user.username, 'speaking:', speaking);
 
-            if(speaking.bitfield == 0) return;
+            if(!user.bot) { //
 
-            // Create a ReadableStream of s16le PCM audio
-            const receiver = connection.receiver.createStream(user, {
-                mode: "pcm",
-                end: "silence"
-            });
+                console.log(user.username, 'speaking:', speaking);
 
-            const writer = receiver.pipe(fs.createWriteStream(`./recorded-${user.id}.pcm`));
-            writer.on("finish", function () {
-                ffmpeg(`./recorded-${user.id}.pcm`)
-                    .inputOptions(['-f s16le', '-ar 48000', '-channels 2'])
-                    .outputOptions(['-f wav', '-ar 16000', '-ac 1'])
-                    .save(`./recorded-${user.id}.wav`)
-                    .on('end', function () {
-                        exec(`python SpeechRec.py recorded-${user.id}.wav`, (error, stdout, stderr) => {
+                if (speaking.bitfield == 0) return;
 
-                            if (error) {
-                                console.error(`error: ${error.message}`);
-                                return;
-                            }
+                // Create a ReadableStream of s16le PCM audio
+                const receiver = connection.receiver.createStream(user, {
+                    mode: "pcm",
+                    end: "silence"
+                });
 
-                            if (stderr) {
-                                console.error(`stderr: ${stderr}`);
-                                return;
-                            }
-                            var output = stdout
-                            if (output.text === "") return;
-                            console.log(user.username, ": ", output.toString());
-                            message.channel.send(user.username + ": " + output.toString());
-                        });
-                    })
-            });
+                const writer = receiver.pipe(fs.createWriteStream(`./recorded-${user.id}.pcm`));
+                writer.on("finish", function () {
+                    var filename = `./recorded-${user.id}.wav`
+                    ffmpeg(`./recorded-${user.id}.pcm`)
+                        .inputOptions(['-f s16le', '-ar 48000', '-channels 2'])
+                        .outputOptions(['-f wav', '-ar 16000', '-ac 1'])
+                        .save(`./recorded-${user.id}.wav`)
+                        .on('end', function () {
+                            exec(`python SpeechRec.py recorded-${user.id}.wav`, (error, stdout, stderr) => {
+
+                                if (error) {
+                                    console.error(`error: ${error.message}`);
+                                    return;
+                                }
+
+                                if (stderr) {
+                                    console.error(`stderr: ${stderr}`);
+                                    return;
+                                }
+                                var output = stdout;
+
+                                if (output.toString() === "") return;
+
+                                console.log(user.username, ": ", output.toString());
+
+                                var JSONinfo = JSON.parse(stdout)
+
+                                if (JSONinfo.toString() === "") return;
+                                //console.log(user.username, ": ", output.text);
+
+
+                                if (JSONinfo.hasOwnProperty('Profanity')) {
+                                    //do stuff if the key exist
+
+                                    var isProfanity = JSONinfo['Profanity'];
+
+                                    if (isProfanity === true || isProfanity === "true") {
+
+                                        if (JSONinfo.hasOwnProperty('Wit')) {
+
+                                            var WitOutput = JSONinfo['Wit'];
+
+                                            const Embed = new Discord.MessageEmbed()
+                                                .setColor('#0099ff')
+                                                .setTitle('Kicked From VoiceChannel')
+                                                .setURL('https://i.kym-cdn.com/photos/images/original/001/299/189/6bb.jpg')
+                                                .setAuthor('Jesus', 'https://st.depositphotos.com/1000975/2477/i/950/depositphotos_24770375-stock-photo-nun-with-gun-isolated-on.jpg')
+                                                .setDescription('Christian Server, no swearing.')
+                                                .setThumbnail('https://pbs.twimg.com/media/ETYqt0eXgAAvw8_?format=jpg&name=4096x4096')
+                                                .addFields(
+                                                    { name: 'Offender', value: user.toString() },
+                                                    { name: 'Offense', value: 'Swearing in VC', inline: true },
+                                                    { name: 'Punishment', value: 'Timeout corner?', inline: true },
+                                                )
+                                                .addField('Speech 2 text value', WitOutput.toString(), true)
+                                                .setImage('https://pbs.twimg.com/media/ETYqt0eXgAAvw8_?format=jpg&name=4096x4096')
+                                                .setTimestamp();
+
+                                            const file = new Discord.MessageAttachment(filename);
+
+                                            message.channel.send({ files: [file], embed: Embed });
+
+                                            ConnectedChannel.members.forEach((member) => {
+                                                if(member.id === user.id){
+                                                    member.voice.kick()
+                                                }
+                                            });
+
+
+
+
+                                        } else {
+
+                                            var SphinxOutput = JSONinfo['Sphinx'];
+
+                                            const Embed = new Discord.MessageEmbed()
+                                                .setColor('#0099ff')
+                                                .setTitle('Kicked From VoiceChannel')
+                                                .setURL('https://i.kym-cdn.com/photos/images/original/001/299/189/6bb.jpg')
+                                                .setAuthor('Jesus', 'https://st.depositphotos.com/1000975/2477/i/950/depositphotos_24770375-stock-photo-nun-with-gun-isolated-on.jpg')
+                                                .setDescription('Christian Server, no swearing.')
+                                                .setThumbnail('https://pbs.twimg.com/media/ETYqt0eXgAAvw8_?format=jpg&name=4096x4096')
+                                                .addFields(
+                                                    { name: 'Offender', value: user.toString() },
+                                                    { name: 'Offense', value: 'Swearing in VC', inline: true },
+                                                    { name: 'Punishment', value: 'Timeout corner?', inline: true },
+                                                )
+                                                .addField('Speech 2 text value', SphinxOutput.toString(), true)
+                                                .setImage('https://pbs.twimg.com/media/ETYqt0eXgAAvw8_?format=jpg&name=4096x4096')
+                                                .setTimestamp();
+
+                                            const file = new Discord.MessageAttachment(filename);
+                                            //var meember = guild.member(user);
+                                            message.channel.send({ files: [file], embed: Embed });
+
+                                            ConnectedChannel.members.forEach((member) => {
+                                                if(member.id === user.id){
+                                                    member.voice.kick()
+                                                }
+                                            });
+                                        }
+
+                                    }
+
+                                }
+
+                            });
+                        })
+                });
+            }
         });
     }
+
 
     if(message.content === '+Run it back'){ // temp command to re-play the listen to the audio that u just said.
         //connection.play('user_audio', {mode: 'opus'});
--- SpeechRec.py
+++ SpeechRec.py
@@ -11,14 +11,15 @@ def use_spinx():
         text = Object_data['Sphinx']
         IsProfanity = profanity.contains_profanity(text)
         Object_data['Profanity'] = IsProfanity
+        Object_data['silence'] = "false"
     except:
-        Object_data['silence']="false"
+        Object_data['silence']="true"
 
 
 
 THRESHOLD = -46
 Object_data = {}
-Object_data['silence'] = "false"
+Object_data['silence'] = "true"
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
 
 r = sr.Recognizer()
@@ -30,13 +31,15 @@ data, rate = sf.read(sys.argv[1]) # load audio
 meter = pyln.Meter(rate) # create BS.1770 meter
 try:
     loudness = meter.integrated_loudness(data)
-    print("Loudness: ", loudness)
+    #print("Loudness: ", loudness)
     if(loudness < THRESHOLD):
-        print("Volume too low or silence, Skipping!")
+        #print("Volume too low or silence, Skipping!")
         Object_data['silence'] = "true"
+        nothing_happened = json.dumps(Object_data)
+        print("{}")
         exit()
 except ValueError:
-    print("Volume too low or silence, Skipping! (In this case file was too small)")
+    #print("Volume too low or silence, Skipping! (In this case file was too small)")
     exit()
 
 
@@ -45,31 +48,49 @@ WIT_AI_KEY = f.read()
 
 try:
     Wit_Output = r.recognize_wit(audio, key=WIT_AI_KEY)
-    print("Wit.ai thinks you said " + Wit_Output)
-    Object_data['Wit']= Wit_Output
+    #print("Wit.ai thinks you said " + Wit_Output)
+
+    if(Wit_Output!=""):
+        Object_data['silence'] = "false"
+        Object_data['Wit']= Wit_Output
+    else:
+        Object_data['silence'] = "true"
+        Object_data['Wit'] = "placeholder"
+
 except sr.UnknownValueError:
     Wit_Output = "Wit.ai could not understand audio"
-    print(Wit_Output)
-    #Object_data['Wit'] = Wit_Output
+    #print(Wit_Output)
+    Object_data['silence'] = "true"
+    Object_data['Wit'] = Wit_Output
 except sr.RequestError as e:
     Wit_Output = "Could not request results from Wit.ai service"
-    print(Wit_Output)
-    #Object_data['Wit'] = Wit_Output
+    Object_data['silence'] = "true"
+    #print(Wit_Output)
+    Object_data['Wit'] = Wit_Output
 
 
 # recognize speech using Sphinx
 try:
     Sphinx_Output = r.recognize_sphinx(audio)
-    print("Sphinx thinks you said " + Sphinx_Output)
-    Object_data['Sphinx'] = Sphinx_Output
+    #print("Sphinx thinks you said " + Sphinx_Output)
+
+    if (Sphinx_Output != ""):
+        Object_data['silence'] = "false"
+        Object_data['Sphinx'] = Sphinx_Output
+    else:
+        Object_data['silence'] = "true"
+        Object_data['Sphinx'] = "placeholder"
+
 except sr.UnknownValueError:
     Sphinx_Output = "Sphinx could not understand audio"
-    print(Sphinx_Output)
-    #Object_data['Sphinx'] = Sphinx_Output
+    #print(Sphinx_Output)
+    Object_data['Sphinx'] = Sphinx_Output
+    Object_data['silence'] = "true"
 except sr.RequestError as e:
     Sphinx_Output = "Sphinx Error"
-    print(Sphinx_Output)
-    #Object_data['Sphinx'] = Sphinx_Output
+    #print(Sphinx_Output)
+    Object_data['Sphinx'] = Sphinx_Output
+    Object_data['silence'] = "true"
 
 try:
     if Object_data['silence']=="false":
@@ -79,8 +100,11 @@ try:
 except:
     use_spinx()
 
-final_json=json.dumps(Object_data)
-print(final_json)
+if(Object_data['silence']!="true"):
+    final_json=json.dumps(Object_data)
+    print(final_json)
+else:
+    print("{}")
 
 
 
